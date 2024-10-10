077364cfebedad0fac1574fc81db8e850a7c2ee4
Peter Robinson <peter.robinson@usask.ca>
2024-10-10 11:09:05 -0600

    add public app config files

---
+++ public/app/config/index.js
@@ -0,0 +1,13 @@
+var _ = require('lodash');
+
+var env = process.env.NODE_ENV;
+
+module.exports = _.assign({
+  env: env,
+  BACKEND_URL: 'http://localhost:3000/api/',
+  IMAGE_UPLOAD_URL: 'http://www.textualcommunities.org/api/upload/',
+  COLLATE_URL: 'http://localhost:8080',
+  IIIF_URL: 'http://www.textualcommunities.org:5004/',
+  host_url: 'http://localhost:3000',
+  authority: 'usask', 
+}, require('./' + env + '.js'));
