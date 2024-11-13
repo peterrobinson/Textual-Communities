2977ec950791bd6562edcfc1262e213bd08f4faa
Peter Robinson <peter.robinson@usask.ca>
2024-11-13 09:04:05 -0700

    excluding data files again 7

--- public/app/data/makeEdition/BOD/common/BOD/witnessesInf.js
+++
@@ -1,3 +0,0 @@
-var witnessInf={};
-witnessInf['Bodley']={id:'Bodley', permission:'No permission found for Bodley Check the documentation'};
-witnessInf['Ff']={id:'Fairfax', permission:'No permission found for Ff Check the documentation'};
