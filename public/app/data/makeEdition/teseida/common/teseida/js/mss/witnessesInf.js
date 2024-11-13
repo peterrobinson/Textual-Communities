6e8d8b1af830c08e67989aa0fc1f5f9adf7d1475
Peter Robinson <peter.robinson@usask.ca>
2024-11-13 08:54:33 -0700

    excluding data files again 5

--- public/app/data/makeEdition/teseida/common/teseida/js/mss/witnessesInf.js
+++
@@ -1,3 +0,0 @@
-var witnessInf={};
-witnessInf['AUT']={id:'The Autograph', permission:'Permissions for the Autograph'};
-witnessInf['NO']={id:'The Naples manuscript', permission:'Permissions for Naples'};
