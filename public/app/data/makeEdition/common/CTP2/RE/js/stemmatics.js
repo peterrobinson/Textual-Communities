d06e4be5e779b47db771b20e9d83ff9a677aa238
Peter Robinson <peter.robinson@usask.ca>
2024-11-13 16:03:06 -0700

    local commit 2

--- public/app/data/makeEdition/common/CTP2/RE/js/stemmatics.js
+++
@@ -1,8 +0,0 @@
-var stemmatics=[
-{"name":"L2", "community":"CTPS","document":"L2", "page":"Stemma"},
-{"name":"RE", "community":"CTPS","document":"RE", "page":"Stemma"},
-{"name":"CO", "community":"CTPS","document":"CO", "page":"Stemma"},
-{"name":"L3", "community":"CTPS","document":"CO", "page":"Stemma"},
-{"name":"WBP", "start":"WBP:line=IRE", "end":"WBP:line=446","community":"CTPS","document":"WBP", "page":"Stemma1"},
-{"name":"WBP", "start":"WBP:line=447", "end":"WBP:line=FRE","community":"CTPS","document":"WBP", "page":"Stemma2"}
-]
--- public/app/data/makeEdition/common/CTP2/WBP/js/aliases.js
+++
@@ -1,5 +0,0 @@
-var aliases=[
-{key: "line", context:"commentary", alias:"Line"},
-{key: "line", context:"menus", alias:""},
-{topEntity:"WBP", context:"all", alias:"Wife of Bath's Prologue"},
-]
